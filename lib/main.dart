0b98ef8a054fb30e71ec1053f29cfe070c2c427c
Moon <joiede7@gmail.com>
2024-02-04 18:55:13 +0900

    charactor project end

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'BBANTO',
       home: Grade(),
     );
@@ -20,7 +21,7 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[600],
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
         title: const Text(
           'BBANTO',
@@ -30,11 +31,23 @@ class Grade extends StatelessWidget {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: const Padding(
+      body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/arisu.gif'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey.shade800,
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
             Text(
               'NAME',
               style: TextStyle(
@@ -45,13 +58,92 @@ class Grade extends StatelessWidget {
             SizedBox(
               height: 10.0,
             ),
-            Text("BBANTO",
+            Text("Arisu",
             style: TextStyle(
               color: Colors.white,
               letterSpacing: 2.0,
               fontSize: 28.0,
               fontWeight: FontWeight.bold,
             ),),
+
+            SizedBox(
+              height: 30.0,
+            ),
+
+            Text(
+              'BBANTO POWER LEVEL',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("14",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),),
+
+            SizedBox(
+              height: 30.0,
+            ),
+
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('face hero tattoo',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/aru.jpg'),
+                radius: 40.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            ),
           ],
         ),
       ),
